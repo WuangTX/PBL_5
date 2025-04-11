29c4167e57108ae75d944f0512da6365ec2b183e
WuangTX <quang9b1234@gmail.com>
2025-04-12 00:51:13 +0700

    Thêm mã nguồn dự án PBL_5 lần 2

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('parked-cars/', views.parked_cars, name='parked_cars'),
+    path('add-vehicle/', views.add_vehicle, name='add_vehicle'),
     
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ def index(request):
     return render(request, 'home/index.html', {'parking_spaces': parking_spaces})
 def parked_cars(request):
     return render(request, 'menu/parked_cars.html')
+def add_vehicle(request):
+    if request.method == 'POST':
+        # Add your logic to save the new parking space
+        return redirect('home')
+    return render(request, 'menu/add_vehicle.html')
